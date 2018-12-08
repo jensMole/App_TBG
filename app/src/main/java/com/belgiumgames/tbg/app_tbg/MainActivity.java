09f36904570a08743f25d3de8f79343b33236d19
jensie1996 <jensmolenaers@hotmail.com>
2018-12-08 21:13:20 +0100

    Added sidebar, Function to open navigation with icon.

--- app/src/main/java/com/belgiumgames/tbg/app_tbg/MainActivity.java
+++ app/src/main/java/com/belgiumgames/tbg/app_tbg/MainActivity.java
@@ -1,13 +1,47 @@
 package com.belgiumgames.tbg.app_tbg;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // gebruik de toolbar als actionbar.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        // Voor het aanmaken van het toggle icoontje.
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        // Icoontje laten draaien.
+        toggle.syncState();
+
+
+    }
+
+    
+    @Override
+    public void onBackPressed() {
+
+        // Als het open staat dan gaan we het sluiten als je op terug drukt.
+        // anders gaan we super oproepen.
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
     }
 }
