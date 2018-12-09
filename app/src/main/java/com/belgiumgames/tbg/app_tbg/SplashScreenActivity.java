d2b9f0646a025bbd4aa07a3292c8144c5e48ddee
jensie1996 <jensmolenaers@hotmail.com>
2018-12-09 18:51:00 +0100

    Toevoegen van foto's en splash screen.

---
+++ app/src/main/java/com/belgiumgames/tbg/app_tbg/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.belgiumgames.tbg.app_tbg;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(3000)
+                .withBackgroundColor(Color.parseColor("#3e3e3e"))
+                .withLogo(R.mipmap.tbg_rond);
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+
+    }
+}
