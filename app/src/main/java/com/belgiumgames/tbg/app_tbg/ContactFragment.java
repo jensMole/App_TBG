3bebfbc16e155586cf05f1b76aaba65f655d9e29
jensie1996 <jensmolenaers@hotmail.com>
2018-12-09 15:17:54 +0100

    Toevoegen van java fragmenten. Aanpassen van main voor het openen van fragmenten.

---
+++ app/src/main/java/com/belgiumgames/tbg/app_tbg/ContactFragment.java
@@ -0,0 +1,20 @@
+package com.belgiumgames.tbg.app_tbg;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContactFragment extends Fragment {
+
+    // Haal het fragment binnen en toon het.
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact, container, false);
+    }
+
+}
--- app/src/main/java/com/belgiumgames/tbg/app_tbg/MainActivity.java
+++ app/src/main/java/com/belgiumgames/tbg/app_tbg/MainActivity.java
@@ -1,13 +1,17 @@
 package com.belgiumgames.tbg.app_tbg;
 
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -22,16 +26,26 @@ public class MainActivity extends AppCompatActivity {
 
         drawer = findViewById(R.id.drawer_layout);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         // Voor het aanmaken van het toggle icoontje.
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         // Icoontje laten draaien.
         toggle.syncState();
 
-
+        // Als je de app opstart toon dan het home scherm en duid het eerste item aan in het menu.
+        // De if statement kijkt na als er iets aanpast aan het systeem zodat deze niet steeds
+        // terug gaat naar home. bv. Bij het draaien van het scherm.
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
     }
 
-    
+
     @Override
     public void onBackPressed() {
 
@@ -43,5 +57,35 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
 
+    }
+
+    // Als je een item klikt in het menu gaan we deze openen en de drawer sluiten.
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            // Commit gaat er voor zorgen zodat het fragment in de container wordt geplaatst.
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.nav_video:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new VideoFragment()).commit();
+                break;
+            case R.id.nav_aankondigingen:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AankondigingenFragment()).commit();
+                break;
+            case R.id.nav_contact:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactFragment()).commit();
+                break;
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
